67cc61a4c89e2ee7451d6a9bc89dff3064c82e38
Tyler Smalley <tyler.smalley@elastic.co>
2021-02-20 18:55:33 -0800

    test
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/jest_pipeline.sh
+++ .buildkite/scripts/jest_pipeline.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# exit immediately on failure, or if an undefined variable is used
 set -eu
 
-# begin the pipeline.yml file
 echo "steps:"
 
 # TODO: add x-pack src
 find packages -name jest.config.js | while read file; do
 cat << EOF
-  - command: "buildkite-agent artifact download Dockerfile . && docker run -it "tylersmalley/kibana-ci:kibana-kibana-build-$BUILDKITE_BUILD_NUMBER" node --expose-gc ./node_modules/.bin/jest --logHeapUsage --runInBand --config $file"
+  - command: "buildkite-agent artifact download Dockerfile . && date"
+    label: ":jest:"
 EOF
 done
