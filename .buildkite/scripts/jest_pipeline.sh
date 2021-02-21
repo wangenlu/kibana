cdc625658f68ee71bc6af83ea9aff32e4c25f511
Tyler Smalley <tyler.smalley@elastic.co>
2021-02-20 19:01:58 -0800

    test
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/jest_pipeline.sh
+++ .buildkite/scripts/jest_pipeline.sh
@@ -4,10 +4,11 @@ set -eu
 
 echo "steps:"
 
-# TODO: add x-pack src
-find packages -name jest.config.js | while read file; do
+find src packages x-pack -name jest.config.js | while read file; do
 cat << EOF
-  - command: "buildkite-agent artifact download Dockerfile . && date"
-    label: ":jest:"
+  - label: ":jest:"
+    command: "docker run -it \"tylersmalley/kibana-ci:kibana-kibana-build-$BUILDKITE_BUILD_NUMBER\" node --expose-gc ./node_modules/.bin/jest --logHeapUsage --runInBand --config $file"
+    plugins:
+      - 'uber-workflow/run-without-clone':
 EOF
 done
